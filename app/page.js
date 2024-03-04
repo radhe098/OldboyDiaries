b77207d113c22c2ef97526b8195ea75150f4d62b
Purushottam Tapase <purushottamtapase1@gmail.com>
2024-03-05 00:41:43 +0530

    initial

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "@/Components/Header"
+import Footer from "@/Components/Footer"
+import Slideshow from '@/Components/Slideshow'
+const page = () => {
+  return (
+    <>
+  <Header/>
+  <Slideshow/>
+  <Footer/>
+    </>
+  )
+}
+
+export default page
