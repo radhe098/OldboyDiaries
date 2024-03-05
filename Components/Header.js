be5e79a7b3fbc1ccf81d54193fae6f6ee467c933
Purushottam Tapase <purushottamtapase1@gmail.com>
2024-03-05 08:05:35 +0530

    checking js

--- Components/Header.js
+++ Components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react'
+// import { useState } from 'react';
 
 const Header = () => {
+ 
   return (
+    <>
     <div className='h-16 px-5 bg-yellow-100 items-center flex justify-between text-xl'>
       
     <h2>Logo</h2>
     <div className='flex gap-4 mr-5'>
+      <a href="http://youtube.com"><button>Youtube</button></a>
       <h3>Bounty</h3>
       <h3>Devs</h3>
       <h3>Docs</h3>
       <h3>About</h3>
     </div>
     </div>
+     
+
+    </>
   )
 }
 
--- Components/Slideshow.jsx
+++ Components/Slideshow.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import '../app/globals.css';
+// "use client";
 // import reactimg from '/assets/simon-hajducki-yGMw4KpX4CE-unsplash.jpg'
 const Slideshow = () => {
+
   return (
     <>
     
@@ -10,11 +12,13 @@ const Slideshow = () => {
         <div className='border-b-black border-1 h-[22rem] w-[auto] flex justify-center    '>
 
         <img src="/src/assets/the secret.jpeg" className='object-cover rounded-3xl mt-3 h-[22rem] min-w-[28rem] font-poppins' alt="" />
+        <h1>the addition of 3 and 4 is {3+4}</h1>
         <div className='discription w-64 h-40 p-3 '>
         The Secret is a 2006 self-help book by Rhonda Byrne, based on the earlier film of the same name. It is based on the belief of the pseudoscientific law of attraction, which claims that thought alone can influence objective circumstances within ones life . The book alleges energy as assurance of its effectiveness.
         </div>
         </div>
-      </div>  
+      </div> 
+      
     </>
   )
 }
